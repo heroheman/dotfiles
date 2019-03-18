efb18a110475e2d947d90354e8825f792c5a1869
Florenz Heldermann <hi@flore.nz>
2019-03-18 20:25:46 +0100

    update dotdrop aliases

--- dotfiles/config/bashscripts/alias_dotdrop.zsh
+++ dotfiles/config/bashscripts/alias_dotdrop.zsh
@@ -7,7 +7,11 @@ alias dotgit="git -C $DOTREPO"
 alias dotsync="dotgit pull && dotgit add -A && dotgit commit && dotgit push; dotdrop install"
 
 
-function dotdrop-import {
+function dot-import {
     $DOTREPO/dotdrop.sh import "$1"
+}
+
+function dot-update {
+    $DOTREPO/dotdrop.sh update "$1"
 }
 
